54ad1efc94319adbcf64df9c4dd57c07607f6d4a
ZolodOV <kengymsen@gmail.com>
2025-11-04 20:01:16 +0700

    update3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@levsib2005/lab6";
+
 export default function Home() {
   const buttonClick = () => {
     console.log("ok")
   }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button variant={"primary"} size={"md"} onClick={buttonClick}>
+        Привет
+      </Button>
     </div>
   );
 }
